9a7155c827d33520ef3f01e24e9f2604c542155e
alvaroloal <alorentealman@gmail.com>
2024-10-09 19:43:05 +0200

    starwarsApi

---
+++ Star-Wars/detalles.js
@@ -0,0 +1,14 @@
+const character = JSON.parse(localStorage.getItem('selectedCharacter'));
+
+if (character) {
+    document.getElementById('characterName').textContent = character.name;
+    document.getElementById('characterDetails').innerHTML = `
+        <p><strong>Altura:</strong> ${character.height} cm</p>
+        <p><strong>Peso:</strong> ${character.mass} kg</p>
+        <p><strong>Color de piel:</strong> ${character.skin_color}</p>
+        <p><strong>Color de cabello:</strong> ${character.hair_color}</p>
+        <p><strong>Color de ojos:</strong> ${character.eye_color}</p>
+        <p><strong>Año de nacimiento:</strong> ${character.birth_year}</p>
+        <p><strong>Género:</strong> ${character.gender}</p>
+    `;
+}
---
+++ Star-Wars/script.js
@@ -0,0 +1,41 @@
+//obtener los personajes de la API
+async function fetchCharacters() {
+    try {
+        const response = await fetch('https://swapi.dev/api/people');
+        const data = await response.json();
+        displayCharacters(data.results);
+    } catch (error) {
+        console.error('Error fetching characters:', error);
+    }
+}
+
+
+function displayCharacters(characters) {
+    const characterDisplay = document.getElementById('characterDisplay');
+    characterDisplay.innerHTML = '';
+
+    characters.forEach(character => {
+        const characterCard = document.createElement('div');
+        characterCard.className = 'col-md-4 mb-4';
+        characterCard.innerHTML = `
+            <div class="card text-center h-1000">
+               <img src="https://starwars-visualguide.com/assets/img/characters/2.jpg" class="card-img-top" alt="${character.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${character.name}</h5>
+                    <button class="btn btn-primary" onclick='displayCharacterDetail(${JSON.stringify(character)})'>Ver</button>
+                </div>
+            </div>
+        `;
+        characterDisplay.appendChild(characterCard);
+    });
+}
+
+
+
+function displayCharacterDetail(character) {
+   
+    localStorage.setItem('selectedCharacter', JSON.stringify(character));
+
+    window.location.href = 'detalles.html';
+}
+window.onload = fetchCharacters;
